cf9a130218c221f2857274b2c07cda472bf4cb53
Kasun <kasun.ranasinghe@icloud.com>
2020-06-14 21:37:03 +0530

    Fixed pgsql issue

--- Sources/Application/Configuration/DBConfiguration.swift
+++ Sources/Application/Configuration/DBConfiguration.swift
@@ -5,7 +5,7 @@ import SwiftKueryPostgreSQL
 
 func initializeDBConfiguratiosn()  {
     
-    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("swift"),.userName("postgres"),.password("postgres")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50))
+    let pool = PostgreSQLConnection.createPool(host: "host.docker.internal", port: 5432, options: [.databaseName("swift"),.userName("postgres"),.password("postgres")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50))
     Database.default = Database(pool)
     
 }
